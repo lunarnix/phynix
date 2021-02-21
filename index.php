2835cd0d3d169c2d58f469d00a52ab39d5e3df60
Lunar <70114078+lunarnix@users.noreply.github.com>
2021-02-21 16:32:06 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<html>
+  <head>
+    <title>Phynix</title>
+  </head>  
 <?php
 echo 'Phynix Discord Bot';
 echo 'Invite @ https://phynix.sylv.ml/invite';
 echo 'Made by LunarNix (Lunar#3455)'
 ?>
+</html>
